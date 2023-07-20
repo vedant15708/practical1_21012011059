7cf2fced8a66836f546fe82f9b86499ced14f431
student <student@gnu.ac.in>
2023-07-20 08:56:36 +0530

    practical1.8,1.9

---
+++ src/main/kotlin/practical1.8.kt
@@ -0,0 +1,23 @@
+import java.util.*
+
+fun main() {
+
+    var x = arrayOf(10, 20, 30)
+    println(Arrays.deepToString(x))
+
+    var y = IntArray(5)
+    y[0] = 12
+    y[1] = 13
+    y[2] = 45
+    y[3] = 75
+    y[4] = 34
+
+    for (i in 0..y.size - 1) {
+        print(" " + y[i])
+    }
+    var z = intArrayOf(10, 34, 44, 61, 6)
+    for (j in 0..z.size - 1) {
+        print(" " + z[i])
+    }
+    var z
+}
