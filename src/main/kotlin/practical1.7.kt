866e8fda4946f40fcb3dbd2412c52a0ef789449c
vedant pansuriya <vedant15708@gmail.com>
2023-07-23 22:47:42 +0530

    practical1.7

---
+++ src/main/kotlin/practical1.7.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val number = 5
+    println("Factorial of $number = ${factorial(number)}")
+}
+
+tailrec fun factorial(n: Int, run: Int = 1): Long {
+    return if (n == 1) run.toLong() else factorial(n-1, run*n)
+}
